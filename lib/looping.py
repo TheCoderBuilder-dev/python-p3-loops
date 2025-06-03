1771c519d696b7a22673cffdac0939f8a72b0858
TheCoderBuilder-dev <brianinesh@gmail.com>
2025-06-04 00:32:10 +0300

    loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    # Count down from 10 to 1 and print "Happy New Year!"
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    # Return a list of squared integers using list comprehension
+    return [num ** 2 for num in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    # Print numbers from 1 to 100 with Fizz, Buzz, and FizzBuzz substitutions
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
